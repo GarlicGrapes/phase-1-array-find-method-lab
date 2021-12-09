b4463dac2e650e953cfd5b9522174151286057ab
Austin Longo <austinlongocontracting@gmail.com>
2021-12-09 14:19:05 -0500

    commit tree ddee337bad10d464d8324c0fbb82b50f68230edd

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+function findWin(array) {
+    return array.result === "W"
+}
+
+
+
+function superbowlWin(rec) {
+    let hasWon = rec.find(findWin)
+    if (typeof hasWon !== 'undefined') {return hasWon.year}   
+}
 
